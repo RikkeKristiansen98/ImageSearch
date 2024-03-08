a22b83dd4882d3a0444e0e953eeaa8b8a6e37808
Rikke Kristiansen <rikke.kristiansen@hotmail.com>
2024-03-08 17:55:47 +0100

    Fixed Router

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,4 +1,15 @@
+import { Outlet } from "react-router-dom";
 export const Layout = () => {
     return (<>
-    </>)
-}
+      <header>
+        <><h1>Header</h1></>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+        <><h1>Footer</h1></>
+      </footer>
+    </>
+  );
+};
